815d4b5bab17e6fb423968c72da2d94e430c980f
Tiago Villalobos <tiagolimavillalobos@gmail.com>
2023-10-24 22:28:56 -0300

    rota para teste simples

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\SMS\TwilioController;
 use App\Http\Controllers\WebSocket\SoketiController;
+use App\Support\Classes\User;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\Route;
 
 
@@ -12,3 +14,19 @@ Route::get('/', function () {
 Route::get('sms/twilio/send', [TwilioController::class, 'send'])->name('sms.twilio.send');
 
 Route::get('soketi', [SoketiController::class, 'index'])->name('websocket.soketi.index');
+
+
+Route::get(
+    uri: 'test', 
+    action: function(){
+
+        $user = new User(
+            name:     'Tiago Villalobos', 
+            email:    'tiagolimavillalobos@gmail.com', 
+            birthday: '21/04/1988'
+        );
+
+        return $user->toJson();
+
+    }
+);
